244c6f28a108cc0e25351d08623661991bd5d24d
jnnfr14 <54405607+jnnfr14@users.noreply.github.com>
2022-01-12 23:26:46 +0700

    menambahkan paragraf

--- Index.php
+++ Index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit suscipit praesentium hic ut, quasi, quisquam, esse corporis unde laborum autem vitae nesciunt dignissimos commodi cupiditate labore! Labore veniam accusamus vel.</p>
 </head>
 <body>
     
