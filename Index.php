52d8c13166448e54c1dbd45173c85fee4259eb19
jnnfr14 <54405607+jnnfr14@users.noreply.github.com>
2022-01-12 23:41:08 +0700

    menambahkan list

--- Index.php
+++ Index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit suscipit praesentium hic ut, quasi, quisquam, esse corporis unde laborum autem vitae nesciunt dignissimos commodi cupiditate labore! Labore veniam accusamus vel.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </head>
 <body>
     
